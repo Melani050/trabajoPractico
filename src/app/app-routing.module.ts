62ccbc204be093408eadc66c9c5b98349d214ec1
Melani050 <tina.meli25@gmail.com>
2023-06-28 11:49:12 -0300

    cree un nuevo componente para las rutas de una vista a la otra y cree un formulario para crear otro articulo -todavia no anda-

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ItemsComponent } from './components/items/items.component';
+import { AdditemComponent } from './components/additem/additem.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    //index tradicional
+    path: '',
+    //propiedad en la que hay que especificar el componente que queremos cargar
+    component: ItemsComponent
+  },
+  {
+    path:'add',
+    component: AdditemComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
